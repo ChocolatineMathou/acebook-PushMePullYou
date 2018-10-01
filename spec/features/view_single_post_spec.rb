c07d962c781e6f19d252a5cae72b986e54bddd85
Malachy Gilchrist <malachygilchrist@gmail.com>
2018-10-01 12:08:34 +0100

    fix: Make sign up helper run correctly

--- spec/features/view_single_post_spec.rb
+++ spec/features/view_single_post_spec.rb
@@ -1,4 +1,4 @@
-require_relative '../support/sign_up'
+require_relative '../../app/helpers/sign_up'
 
 RSpec.configure do |config|
   config.include SignUpHelper
@@ -7,7 +7,6 @@ end
 RSpec.feature 'Single view', type: :feature do
   scenario 'User can see a single post' do
     sign_up
-    expect(sign_up).to be(:available)
 
   end
 end
