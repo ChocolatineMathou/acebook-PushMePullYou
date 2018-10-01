800b11b1f8e187adac453d1a644f99f314e77a71
davidlawes <davidlawes@hotmail.co.uk>
2018-10-01 16:49:39 +0100

    style: added css for delete link and hover effects for all other links too

--- spec/features/user_can_delete_posts_spec.rb
+++ spec/features/user_can_delete_posts_spec.rb
@@ -15,8 +15,8 @@ RSpec.feature "Delete Posts", type: :feature do
 
   scenario 'A signed in user can delete their post' do
     expect(page).to have_content 'test'
-    expect(page).to have_button 'post.delete?'
-    click_button 'post.delete?'
+    expect(page).to have_link 'post.delete?'
+    click_link 'post.delete?'
     expect(page).not_to have_content 'test'
     expect(URI.parse(current_url).path).to eq '/posts'
   end
@@ -31,7 +31,7 @@ RSpec.feature "Delete Posts", type: :feature do
     fill_in 'customer_password_confirmation', with: 'test2123'
     click_button 'Sign up'
     click_link 'get :posts'
-    expect(page).not_to have_button 'post.delete?'
+    expect(page).not_to have_link 'post.delete?'
   end
 
 end
