735bf3f66bb5e507a53a97da84a60a4bfc65cdea
Malachy Gilchrist <malachygilchrist@gmail.com>
2018-10-01 16:51:24 +0100

    tests: Add test for two users like same post

--- app/helpers/sign_up.rb
+++ app/helpers/sign_up.rb
@@ -1,11 +1,11 @@
 module SignUpHelper
-  def sign_up
+  def sign_up(first_name, last_name, email, password)
     visit '/customers/sign_up'
-    fill_in 'customer_first_name', with: 'PushMe'
-    fill_in 'customer_last_name', with: 'PushYou'
-    fill_in 'customer_email', with: 'test@test.com'
-    fill_in 'customer_password', with: 'test123'
-    fill_in 'customer_password_confirmation', with: 'test123'
+    fill_in 'customer_first_name', with: first_name
+    fill_in 'customer_last_name', with: last_name
+    fill_in 'customer_email', with: email
+    fill_in 'customer_password', with: password
+    fill_in 'customer_password_confirmation', with: password
     click_button 'Sign up'
   end
 end
--- spec/features/like_post_spec.rb
+++ spec/features/like_post_spec.rb
@@ -9,7 +9,7 @@ end
 RSpec.feature 'Like a post', type: :feature do
 
   before(:each) do
-    sign_up
+    sign_up('PushMe', 'PullYou', 'test@test.com', '123456')
     make_post
     click_link 'Like'
     @post = Post.all[0]
@@ -22,5 +22,14 @@ RSpec.feature 'Like a post', type: :feature do
   scenario 'User can only like a post once' do
     click_link 'Like'
     expect(@post.likes.count).to eq(0)
+  end
+
+  scenario 'Two users can like the same post' do
+    visit '/'
+    click_link 'get :sign_out'
+    sign_up('Mal', 'Gil', 'mal.gil@test.com', '123456')
+    visit '/posts'
+    click_link 'Like'
+    expect(@post.likes.count).to eq(2)
   end
 end
