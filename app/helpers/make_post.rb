62d99fbb4a9c88c9394541f20f6c56aab875e476
Malachy Gilchrist <malachygilchrist@gmail.com>
2018-10-01 13:18:18 +0100

    fix: Change comment test to like test

--- app/helpers/make_post.rb
+++ app/helpers/make_post.rb
@@ -1,5 +1,7 @@
 module MakePostHelper
   def make_post
     visit '/posts/new'
+    fill_in 'post_message', with: 'test post'
+    click_button 'Submit'
   end
 end
--- spec/features/like_post_spec.rb
+++ spec/features/like_post_spec.rb
@@ -1,12 +1,17 @@
 require_relative '../../app/helpers/sign_up'
+require_relative '../../app/helpers/make_post'
 
 RSpec.configure do |config|
   config.include SignUpHelper
+  config.include MakePostHelper
 end
 
 RSpec.feature 'Single view', type: :feature do
   scenario 'User can see a single post' do
     sign_up
-
+    make_post
+    click_button 'comment'
+    fill_in 'post_comment', with: 'test comment'
+    expect(page).to have_content('test comment')
   end
 end
