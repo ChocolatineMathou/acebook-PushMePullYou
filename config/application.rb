001e6b05d0c4b718005b347edb878dd22c12d696
Mathilde Ferrand Coeurderoy <mathilde.ferrand.coeurderoy@gmail.com>
2018-10-02 13:51:07 +0100

    chore/initialise API only

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Acebook
   class Application < Rails::Application
+    confit.api_only = true
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
