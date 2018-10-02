83cec493c124c0221ec95f8d87984e89fbd79462
Mathilde Ferrand Coeurderoy <mathilde.ferrand.coeurderoy@gmail.com>
2018-10-02 16:47:59 +0100

    tests: add invalid password context and refactor

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,18 @@
 class SessionsController < Devise::SessionsController
 
   def create
-    # validate that email+>password exists in database
     customer = Customer.find_by(email: params[:email])
 
     if !!customer
-      sign_in customer
-      render :json => { :success=>true, current_customer: current_customer }
+      if customer.valid_password?(params[:password])
+        sign_in customer
+        render :json => { :success=>true, current_customer: current_customer }
+      else
+        render :json => { :success=>false, message: "Invalid email or password"}
+      end
     else
-      render :json => { :success=>false, message: "Invalid email or password"}
+      render :json => { :success=>false, message: "Account doesn't exist"}
     end
-  end
-
-  private
-
-  def sign_in_params
-    params.require(:customer).permit(:email, :password)
   end
 
 end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -29,7 +29,7 @@ RSpec.describe SessionsController, type: :controller do
       end
     end
 
-    context "given invalid sign in details" do
+    context "given invalid email" do
       it "responds in json format" do
         post :create, params: {email: "test@test.com", password: "123456"}
         expect(response).to be_successful
@@ -39,6 +39,7 @@ RSpec.describe SessionsController, type: :controller do
       it "provides an unsuccessful indication" do
         post :create, params: {email: "test@test.com", password: "123456"}
         expect(JSON.parse(response.body)["success"]).to eq false
+        expect(JSON.parse(response.body)["message"]).to eq "Account doesn't exist"
       end
 
       it "does not assign customer to current_customer" do
@@ -46,6 +47,15 @@ RSpec.describe SessionsController, type: :controller do
         expect(subject.current_customer).to eq nil
       end
     end
+
+    context "given invalid password" do
+      it "provides an unsuccessful indication" do
+        post :create, params: {email: "testing@rspec.com", password: "654321"}
+        expect(JSON.parse(response.body)["success"]).to eq false
+        expect(JSON.parse(response.body)["message"]).to eq "Invalid email or password"
+      end
+    end
+
   end
 
 end
