8693d669479668472101b9e88571fb06567ab364
Mathilde Ferrand Coeurderoy <mathilde.ferrand.coeurderoy@gmail.com>
2018-10-02 17:30:03 +0100

    chore: convert registrations controller for API

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,13 +1,21 @@
 class RegistrationsController < Devise::RegistrationsController
 
+  def create
+    customer = Customer.create({
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation]})
+
+    sign_in customer
+    render :json => {:success=>true, current_customer: current_customer }
+
+  end
+
   private
 
   def sign_up_params
-    p "sign_up_params"
     params.require(:customer).permit(:first_name, :last_name, :email, :password, :password_confirmation)
-  end
-
-  def account_update_params
-    params.require(:customer).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password)
   end
 end
--- spec/controllers/registrations_controller_spec.rb
+++ spec/controllers/registrations_controller_spec.rb
@@ -2,16 +2,31 @@ require 'rails_helper'
 
 RSpec.describe RegistrationsController, type: :controller do
 
-  let(:params) {{ first_name: "rspec", last_name: "testing", email: "testing@rspec.com", password: "123456", password_confirmation: "123456" }}
+  #registrations#create
+  # /customers
 
-  # registrations#new
-  # /customers/sign_up
-  describe "GET /" do
-
+  describe "POST /" do
     context "with valid params" do
+
+      before do
+        @request.env["devise.mapping"] = Devise.mappings[:customer]
+      end
+
       it "creates a new customer" do
+        post :create, params: { first_name: "test", last_name: "rspec", email: "testing@rspec.com", password: "123456", password_confirmation: "123456"}
+        expect(response).to be_successful
+        expect(response.content_type).to eq('application/json')
+      end
 
+      it "provides a success indication" do
+        post :create, params: { first_name: "test", last_name: "rspec", email: "testing@rspec.com", password: "123456", password_confirmation: "123456"}
+        expect(JSON.parse(response.body)["success"]).to eq true
+        expect(JSON.parse(response.body)["current_customer"]["email"]).to eq "testing@rspec.com"
+      end
 
+      it "assigns customer to current_customer" do
+        post :create, params: { first_name: "test", last_name: "rspec", email: "testing@rspec.com", password: "123456", password_confirmation: "123456"}
+        expect(subject.current_customer.email). to eq "testing@rspec.com"
       end
     end
 
