7c62f34e43df00a91b9e414c34685aa8ded3a42d
davidlawes <davidlawes@hotmail.co.uk>
2018-10-04 12:00:41 +0100

    chore: changed folder structure for backend, checked rspec and rails server still functions as expected

---
+++ backend/app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < Devise::SessionsController
+
+  # overrides devises methods for checking if user is already signed out (prevents flash messages)
+  skip_before_action :verify_signed_out_user
+
+  def create
+    customer = Customer.find_by(email: params[:email])
+
+    if !!customer
+      if customer.valid_password?(params[:password])
+        sign_in customer
+        render :json => { :success=>true, current_customer: current_customer, session: session["warden.user.customer.key"] }
+      else
+        render :json => { :success=>false, current_customer: nil, session: nil, message: "Invalid email or password"}
+      end
+    else
+      render :json => { :success=>false, current_customer: nil, session: nil, message: "Account doesn't exist"}
+    end
+  end
+
+  def destroy
+    sign_out current_customer
+    render :json => { :success=>true, current_customer: nil, session: nil }
+  end
+
+end
---
+++ backend/config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+
+  get 'pages/home'
+
+  devise_for :customers, :controllers => { registrations: 'registrations', sessions: 'sessions' }
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :posts
+
+  resources :posts do
+    resources :likes
+  end
+
+
+  root 'pages#home'
+end
